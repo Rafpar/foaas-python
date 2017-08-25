6f153d7962eb54203570fcaab5420e173cdf8259
Rafpar <pardarafal@gmail.com>
2017-08-25 15:32:10 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import redirect
 import applyfuck
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return applyfuck.applyapp()
+    return redirect(str(applyfuck.applyapp()))
 
 if __name__ == "__main__":
     application.run()
