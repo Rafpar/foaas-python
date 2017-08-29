bff4ba98511ec39332173dcfa034ddad47b18c32
Rafał Parda <pardarafal@gmail.com>
2017-08-29 10:39:16 +0200

    update for dev branch

--- applyfuck.py
+++
@@ -1,7 +0,0 @@
-from foaas import fuck
-
-
-def applyapp():
-    m = fuck.random(from_='Rafal').url
-    print m
-    return m
--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,28 @@
 from flask import Flask
 from flask import redirect
-import applyfuck
+import random
 
 application = Flask(__name__)
 
 
 @application.route("/")
 def hello():
-    return redirect(str(applyfuck.applyapp()))
+
+    ramdomList1 = ["Artless", "Bawdy", "Beslubbering"]
+    randomList2 = ["Base-court", "Bat-fowling", "Beef-witted"]
+    randomList3 = ["Apple-john", "Baggage", "Barnacle"]
+    vowels = "AEIOU"
+    article = "a"
+
+    firstWord = random.choice(ramdomList1)
+    secondWorld = random.choice(randomList2)
+    thirdWord = random.choice(randomList3)
+    for x in vowels:
+        if firstWord[0] == x:
+            article = "an"
+    print "Thou art {0} {1} {2} {3}!".format(article, firstWord, secondWorld, thirdWord)
 
 
 if __name__ == "__main__":
     application.run()
+
